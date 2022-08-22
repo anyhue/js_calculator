2c0f61cf5428891e933547f0eab226664fb52097
anyhue <111332914+anyhue@users.noreply.github.com>
2022-08-22 12:49:25 +0900

    js calculator

---
+++ app.js
@@ -0,0 +1,19 @@
+const calculator = {
+    add: function (a, b) {
+        console.log(a+b);
+    },
+    minus: function (a, b) {
+        console.log(a-b);
+    },
+    devide: function (a, b) {
+        console.log(a/b);
+    },
+    powerof: function (a, b) {
+        console.log(a**b);
+    }
+    }
+
+    calculator.add(5, 3);
+    calculator.minus(5,3);
+    calculator.devide(5,3);
+    calculator.powerof(5,3);
